da725ab2da1adcb128d2145aacf126448478531f
Adam-Greenfield <ajamesgreenfield@gmail.com>
2015-11-23 13:52:33 +0000

    added group show and index

--- api/controllers/groups_controller.js
+++ api/controllers/groups_controller.js
@@ -1,21 +1,27 @@
 var Group = require('../models/group');
 
-function getGroups(req, res){
+function groupsIndex(req, res){
+  Group.find(function(err, groups){
+    if (err) return res.status(404).json({message: 'Something went wrong and we could not pull the groups.'});
+    res.status(200).json({ groups: groups });
+  });
+};
+
+function groupsShow(req, res){
+  Group.findById(req.params.id, function(err, group){
+    if (err) return res.status(404).json({message: 'Something went wrong and we could not find the group.'});
+    res.status(200).json({ group: group });
+  });
+};
+
+function groupsCreate(req, res){
 
 }
 
-function showGroup(req, res){
+function groupsUpdate(req, res){
 
 }
 
-function createGroup(req, res){
+function groupsDestroy(req, res){
 
-}
-
-function updateGroup(req, res){
-
-}
-
-function destroyGroup(req, res){
-  
 }
