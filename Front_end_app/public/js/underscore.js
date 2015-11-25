651b311ea4c6367df681411395811cc1b341e375
Adam-Greenfield <ajamesgreenfield@gmail.com>
2015-11-25 20:58:47 +0000

    added underscore.js

---
+++ Front_end_app/public/js/underscore.js
@@ -0,0 +1,37 @@
+$(function(){
+  var Views = Views || {}
+  window.Views = Views
+  Views.render = function(path, data, selector){
+    $.ajax({url: path}).done(function(stringTemplate){
+      var underscoreTemplate = _.template(stringTemplate);
+      compiledTemplate = underscoreTemplate(data);
+      $(selector).html(compiledTemplate);
+    })
+  }
+
+  Views.renderCollection = function(path, collection, selector){
+
+    $.ajax({url: path}).done(function(stringTemplate){
+      var underscoreTemplate = _.template(stringTemplate);
+      _(collection).each(function(item){
+        compiledTemplate = underscoreTemplate(item);
+        $(selector).append(compiledTemplate);
+      })
+      
+    })
+  }
+
+  // Views.render("/templates/layout.html", null, "body");
+
+
+
+
+
+
+
+
+
+
+
+
+});
